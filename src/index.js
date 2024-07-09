0d3410c81d95427af4ca4b0d8e63b8b59be178bd
teshager <teshager8922@gmail.com>
2024-07-09 17:21:52 +0300

    put created todos into the todolist

--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
+ const todoList={
+
+ };
+ 
  const ToDo=(title,description,dueDate,priority)=>{
- title=title;
- description=description;
+    title=title;
+    description=description;
     dueDate =dueDate;
- priority=priority;
- status;
-const assesStatusofToDo=()=>{
+    priority=priority;
+ let status;
+const assesStatusofToDo=(()=>{
     //overdue
     //on progress
     //Done
-}
+    const today=Date.now();
+    const dueDateFormatted= new Date(dueDate).getTime();
+    if(dueDateFormatted<today && status!=='done') status="overdue";
+    if(dueDateFormatted>today && status!=='done') status='on progress';
+})();
  const getToDo=()=>{
-    return {title,description,dueDate,priority}
+    return {title,description,dueDate,priority,status}
  }
  return {getToDo}
  }
 
- let todo =ToDo("clean the room","use the broom and the brush",'1/1/2024','very important');
- console.log(todo.getToDo());
+ let todo =ToDo("clean the room","use the broom and the brush",'1/1/2025','very important');
+ let todo1 =ToDo("clean the room well","use the broom and the brush",'1/1/2025','very important');
+ todoList[`${Date.now()}`]=todo.getToDo();
+ todoList[`${Date.now()}`]=todo1.getToDo();
+ console.log(Object.keys(todoList).length)
