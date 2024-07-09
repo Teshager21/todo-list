3a42b515a2d0e738a336ae5bf710f5848ba17e4d
teshager <teshager8922@gmail.com>
2024-07-09 16:17:01 +0300

    write a todo factory function that returns a to do object

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log("Hello World!");
+ const ToDo=(title,description,dueDate,priority)=>{
+ title=title;
+ description=description;
+    dueDate =dueDate;
+ priority=priority;
+ status;
+const assesStatusofToDo=()=>{
+    //overdue
+    //on progress
+    //Done
+}
+ const getToDo=()=>{
+    return {title,description,dueDate,priority}
+ }
+ return {getToDo}
+ }
+
+ let todo =ToDo("clean the room","use the broom and the brush",'1/1/2024','very important');
+ console.log(todo.getToDo());
