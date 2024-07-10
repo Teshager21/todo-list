6de263f3734840c55523c078122da474682a1ed2
teshager <teshager8922@gmail.com>
2024-07-10 07:31:54 +0300

    include an add and delete method for todolists using localStorage as database

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
 const { v4: uuidv4 } = require('uuid');
+if(!localStorage.getItem('todoList')){
+    localStorage.setItem('todoList',JSON.stringify({}));
+    console.log('here here....')
+}
+   
  const todoList={
 
+addToDo:function(todo){
+    let cache=JSON.parse(localStorage.getItem('todoList'));
+    cache[`${uuidv4()}`]=todo;
+    localStorage.setItem('todoList',JSON.stringify(cache));
+
+},
+
+updateToDo:function(new_title,new_description,new_dueDate,new_priority){
+        title=new_title;
+        description=new_description;
+        dueDate=new_dueDate;
+        priority=new_priority;
+    },
+deleteToDo:function(id){
+        let cache=JSON.parse(localStorage.getItem('todoList'));
+        delete cache[id];
+        localStorage.setItem('todoList',JSON.stringify(cache));
+
+    }
  };
  
  const ToDo=(title,description,dueDate,priority)=>{
@@ -9,6 +33,7 @@ const { v4: uuidv4 } = require('uuid');
     dueDate =dueDate;
     priority=priority;
  let status;
+ let todoList={};
 const assesStatusofToDo=(()=>{
     //overdue
     //on progress
@@ -18,14 +43,25 @@ const assesStatusofToDo=(()=>{
     if(dueDateFormatted<today && status!=='done') status="overdue";
     if(dueDateFormatted>today && status!=='done') status='on progress';
 })();
+const updateStatusofToDo=(status)=>{
+    status=status;
+}
+
+
  const getToDo=()=>{
     return {title,description,dueDate,priority,status}
+ }
+ const deleteToDO=()=>{
+
  }
  return {getToDo}
  }
 
  let todo =ToDo("clean the room","use the broom and the brush",'1/1/2025','very important');
  let todo1 =ToDo("clean the room well","use the broom and the brush",'1/1/2025','very important');
- todoList[`${uuidv4()}`]=todo.getToDo();
- todoList[`${uuidv4()}`]=todo1.getToDo();
- console.log(todoList)
+
+todoList.addToDo(todo.getToDo());
+todoList.addToDo(todo1.getToDo())
+todoList.deleteToDo(Object.keys(JSON.parse(localStorage.getItem('todoList')))[0]);
+ console.log(JSON.parse(localStorage['todoList']));
+
