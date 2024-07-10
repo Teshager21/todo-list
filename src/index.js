6525a8ddab8dcbd87559a8ad893d346d0c408d94
teshager <teshager8922@gmail.com>
2024-07-10 09:13:17 +0300

    refactor a repeated code in the todoList object

--- src/index.js
+++ src/index.js
@@ -1,28 +1,29 @@
 const { v4: uuidv4 } = require('uuid');
 if(!localStorage.getItem('todoList')){
     localStorage.setItem('todoList',JSON.stringify({}));
-    console.log('here here....')
 }
-   
- const todoList={
+  
+const todoList={
+cache:JSON.parse(localStorage.getItem('todoList')),
+updateStorage:function(){
+    localStorage.setItem('todoList',JSON.stringify(this.cache)); 
+},
+addToDo:function(todo){ 
+    this.cache[`${uuidv4()}`]=todo;
+    this.updateStorage();
+},
 
-addToDo:function(todo){
-    let cache=JSON.parse(localStorage.getItem('todoList'));
-    cache[`${uuidv4()}`]=todo;
-    localStorage.setItem('todoList',JSON.stringify(cache));
-
+readToDo:function(id){
+    return this.cache[id];
 },
 
 updateToDo:function(id,new_title,new_description,new_dueDate,new_priority,new_status){
-    let cache=JSON.parse(localStorage.getItem('todoList'));
-    cache[id]=ToDo(new_title,new_description,new_dueDate,new_priority,new_status).getToDo();
-    localStorage.setItem('todoList',JSON.stringify(cache));   
+    this.cache[id]=ToDo(new_title,new_description,new_dueDate,new_priority,new_status).getToDo();
+    this.updateStorage();
     },
 deleteToDo:function(id){
-        let cache=JSON.parse(localStorage.getItem('todoList'));
-        delete cache[id];
-        localStorage.setItem('todoList',JSON.stringify(cache));
-
+        delete this.cache[id];
+        this.updateStorage();
     }
  };
  
@@ -32,27 +33,31 @@ deleteToDo:function(id){
     dueDate =dueDate;
     priority=priority;
     status=status;
-const assesStatusofToDo=(()=>{
-    const today=Date.now();
-    const dueDateFormatted= new Date(dueDate).getTime();
-    if(dueDateFormatted<today && status!=='done') status="overdue";
-    if(dueDateFormatted>today && status!=='done') status='on progress';
-})();
-const updateStatusofToDo=(status)=>{
-    status=status;
-}
- const getToDo=()=>{
-    return {title,description,dueDate,priority,status}
- }
- return {getToDo}
+    const assesStatusofToDo=(()=>{
+        const today=Date.now();
+        const dueDateFormatted= new Date(dueDate).getTime();
+        if(dueDateFormatted<today && status!=='done') status="overdue";
+        if(dueDateFormatted>today && status!=='done') status='on progress';
+    })();
+    const updateStatusofToDo=(status)=>{
+        status=status;
+    }
+    const getToDo=()=>{
+        return {title,description,dueDate,priority,status}
+    }
+    return {getToDo}
  }
 
- let todo =ToDo("clean the room","use the broom and the brush",'1/1/2025','very important');
- let todo1 =ToDo("clean the room well","use the broom and the brush",'1/1/2025','very important');
+ const updateStorage=(cache)=>{
+    localStorage.setItem('todoList',JSON.stringify({}));
+ }
 
-todoList.addToDo(todo.getToDo());
-todoList.addToDo(todo1.getToDo())
+todoList.addToDo(ToDo("clean the room","use the broom and the brush",'1/1/2025','very important').getToDo());
+todoList.addToDo(ToDo("clean the room well","use the broom and the brush",'1/1/2025','very important').getToDo())
+console.log('before deleting',todoList.readToDo(Object.keys(JSON.parse(localStorage.getItem('todoList')))[0]));
 todoList.deleteToDo(Object.keys(JSON.parse(localStorage.getItem('todoList')))[0]);
 todoList.updateToDo(Object.keys(JSON.parse(localStorage.getItem('todoList')))[0],"do 5S on the room","use the broom and the brush",'1/1/2025','very important')
  console.log(JSON.parse(localStorage['todoList']));
+ 
+
 
